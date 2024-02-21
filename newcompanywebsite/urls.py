5261530c61af166c68de13ee1961bc41c7df0742
Prajjwal Acharya <34410880+prajjwalz1@users.noreply.github.com>
2024-02-21 16:35:07 +0545

    Update urls.py

--- newcompanywebsite/urls.py
+++ newcompanywebsite/urls.py
@@ -20,3 +20,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app.urls'))
 ]
+
+# add at the last
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
