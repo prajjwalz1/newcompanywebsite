a1c09e8283c05e782f79caa47beac809da525916
Prajjwal Acharya <34410880+prajjwalz1@users.noreply.github.com>
2024-02-21 16:11:39 +0545

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -10,6 +10,6 @@
 
  # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+echo yes | python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
