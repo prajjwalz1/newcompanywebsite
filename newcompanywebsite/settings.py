873fef0dba227c21d8ee4661046d270e3d0650fd
Prajjwal Acharya <34410880+prajjwalz1@users.noreply.github.com>
2024-02-21 17:00:40 +0545

    Update settings.py

--- newcompanywebsite/settings.py
+++ newcompanywebsite/settings.py
@@ -151,8 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/vercel/path0/static'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static'
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 MEDIA_URL="/media/"
 # Default primary key field type
