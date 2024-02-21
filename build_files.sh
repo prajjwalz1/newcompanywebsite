8bd5cd062b601fb04ef88b50825df5e3d10dc4ae
Prajjwal Acharya <34410880+prajjwalz1@users.noreply.github.com>
2024-02-21 15:11:36 +0545

    Create build_files.sh

---
+++ build_files.sh
@@ -0,0 +1,15 @@
+ echo "BUILD START"
+ # python3.9 -m pip install -r requirements.txt
+ python3.9 -m venv venv
+    source venv/bin/activate
+ pip install -r requirements.txt
+ # python3.9 manage.py collectstatic
+ # ls
+ echo "BUILD END"
+# source venv/bin/activate
+
+ # Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic
+python manage.py makemigrations
+python manage.py migrate
