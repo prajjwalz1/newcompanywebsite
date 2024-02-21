6a0475ffbf1dea4f0452cf1c8087623fe70c532d
Prajjwal Acharya <34410880+prajjwalz1@users.noreply.github.com>
2024-02-21 16:02:16 +0545

    Update settings.py

--- newcompanywebsite/settings.py
+++ newcompanywebsite/settings.py
@@ -80,13 +80,7 @@ WSGI_APPLICATION = 'newcompanywebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
-# ,
-#     }
-# }
+
 
 DATABASES = {
     'default': {
